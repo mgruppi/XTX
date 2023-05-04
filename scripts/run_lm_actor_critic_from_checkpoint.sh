557433d8cece1385005700cfe5c4852b91f534bc
mgruppi <mauricio.gruppi@gmail.com>
2023-05-03 20:51:49 -0400

    change memory size

--- scripts/run_lm_actor_critic_from_checkpoint.sh
+++ scripts/run_lm_actor_critic_from_checkpoint.sh
@@ -5,7 +5,9 @@ JERICHO_SEED=$SEED # set to -1 if you want stochastic version
 MODEL_NAME='lm_actor_critic'
 JERICHO_ADD_WT='add_wt' # change to 'no_add_wt' if you don't want to add extra actions to game grammar
 
-MEMORY_SIZE=100000  # Override memory_size
+WEIGHTS_FILE="XTX/wandb/run-20230501_235619-12igeyka/files/weights_9.pt"
+MEMORY_FILE="XTX/wandb/run-20230501_235619-12igeyka/files/memory_9.pkl"
+RUN_ID="12igeyka"
 
 python3 -m scripts.train_rl --output_dir logs/${LOG_FOLDER} \
                     --rom_path games/${GAME} \
@@ -14,4 +16,6 @@ python3 -m scripts.train_rl --output_dir logs/${LOG_FOLDER} \
                     --model_name ${MODEL_NAME} \
                     --eval_freq 10000000 \
                     --jericho_add_wt ${JERICHO_ADD_WT} \
-                    --memory_size ${MEMORY_SIZE}
+                    --weight_file "$WEIGHT_FILE" \
+                    --memory_file "$MEMORY_FILE" \
+                    --run_id "$RUN_ID"
