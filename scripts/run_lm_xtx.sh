c68004e530fd2aa83f0fb01c6e7008c1302de267
mgruppi <mauricio.gruppi@gmail.com>
2023-06-09 13:13:07 -0400

    Update script with `LM_MODEL`

--- scripts/run_lm_xtx.sh
+++ scripts/run_lm_xtx.sh
@@ -4,6 +4,7 @@ SEED=0
 JERICHO_SEED=$SEED # set to -1 if you want stochastic version
 MODEL_NAME='lm_xtx'
 JERICHO_ADD_WT='add_wt' # change to 'no_add_wt' if you don't want to add extra actions to game grammar
+LM_MODEL="all-MiniLM-L6-v2"
 
 # NOTE: r_for below corresponds to alpha_1 in the paper
 
@@ -43,3 +44,4 @@ python3 -m scripts.train_rl --output_dir logs/${LOG_FOLDER} \
                     --il_use_dropout 1 \
                     --traj_dropout_prob 0.005 \
                     --jericho_add_wt ${JERICHO_ADD_WT}
+                    --lm_model_name ${LM_MODEL}
